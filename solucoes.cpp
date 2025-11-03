a62f1bcb503bb404b211f9b43b5399b06765d9d7
Victor <victor@medtechanalytics.com>
2025-11-03 16:53:33 -0300

    setup inicial do projeto

---
+++ solucoes.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+bool sao_anagramas(std::string string1, std::string string2) {
+    return true;
+}
+
+std::string cifra_de_cesar(std::string texto, int deslocamento) {
+    return texto;
+}
+
+bool valida_cpf(std::string cpf_string) {
+    return true;
+}
