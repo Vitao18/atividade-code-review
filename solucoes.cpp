690319625c15eb0055d80b4d948a5c187b0f76aa
gutrufsophia <sophiagutruf@usp.br>
2025-11-06 20:56:39 -0300

    feat: implementa logica da funcao de anagrama

--- solucoes.cpp
+++ solucoes.cpp
@@ -1,13 +1,33 @@
 #include <string>
+#include <set>
 
-bool sao_anagramas(std::string string1, std::string string2) {
+using namespace std;
+
+bool sao_anagramas(string string1, string string2) {
+    if (string1.length() != string2.length()) {
+        return false; // anagramas necessariamente tem o mesmo tamanho
+    }
+
+    set<char> char_set1;
+    set<char> char_set2;
+
+    for (char c : string1) {
+        char_set1.insert(c);
+    }
+
+    for (char c : string2) {
+        if (char_set1.find(c) == char_set1.end()) {
+            return false; // char na string2 não encontrado na string1
+        }
+    }
+    
     return true;
 }
 
-std::string cifra_de_cesar(std::string texto, int deslocamento) {
+string cifra_de_cesar(string texto, int deslocamento) {
     return texto;
 }
 
-bool valida_cpf(std::string cpf_string) {
+bool valida_cpf(string cpf) {
     return true;
 }
