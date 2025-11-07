5b519a804e93410fc760fa524602c2b135d70232
Enzo Spinella <49032802+enzospinella@users.noreply.github.com>
2025-11-06 23:28:45 -0300

    Outros testes exemplos adicionados

--- solucoes.cpp
+++ solucoes.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <set>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -33,9 +34,32 @@ bool valida_cpf(string cpf) {
     return true;
 }
 
+void teste_sao_anagramas() {
+    // Casos básicos
+    assert(sao_anagramas("amor", "roma") == true);
+    assert(sao_anagramas("alegria", "galeria") == true);
+    assert(sao_anagramas("", "") == true); 
+    assert(sao_anagramas("pato", "patos") == false);
+    assert(sao_anagramas("casa", "ca") == false);
+    assert(sao_anagramas("gato", "rato") == false); 
+    assert(sao_anagramas("banana", "bananana") == false);
+    assert(sao_anagramas("a b c", "c b a") == true);
+
+    // Casos em que o set falha
+    assert(sao_anagramas("banana", "nababa") == false);
+    assert(sao_anagramas("aab", "abb") == false);
+
+    // Caso que a função diferencia maiúsculas de minúsculas
+    assert(sao_anagramas("Amor", "roma") == true); 
+
+    // Caso que mostra que a função não ignora espaços e que o set falha
+    assert(sao_anagramas("a b c", "abc  ") == false);
+
+}
+
 int main() {
     // testando a função de anagramas
-    cout << sao_anagramas("roma", "amor") << endl;
+    teste_sao_anagramas();
 
     
     return 0;
