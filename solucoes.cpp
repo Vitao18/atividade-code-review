1265e0857629586b6584293daf035017c7fccecf
gutrufsophia <sophiagutruf@usp.br>
2025-11-07 10:33:59 -0300

    correção: todas as solicitações corrigidas tanto no anagrama quanto no cesar

--- solucoes.cpp
+++ solucoes.cpp
@@ -1,28 +1,23 @@
 #include <string>
 #include <set>
 #include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cassert>
 
 using namespace std;
 
 bool sao_anagramas(string string1, string string2) {
-    if (string1.length() != string2.length()) {
-        return false; // anagramas necessariamente tem o mesmo tamanho
-    }
+    if (string1.length() != string2.length()) return false;
 
-    set<char> char_set1;
-    set<char> char_set2;
+   
+    transform(string1.begin(), string1.end(), string1.begin(), [](unsigned char c){ return std::tolower(c); });
+    transform(string2.begin(), string2.end(), string2.begin(), [](unsigned char c){ return std::tolower(c); });
 
-    for (char c : string1) {
-        char_set1.insert(c);
-    }
+    sort(string1.begin(), string1.end());
+    sort(string2.begin(), string2.end());
 
-    for (char c : string2) {
-        if (char_set1.find(c) == char_set1.end()) {
-            return false; // char na string2 não encontrado na string1
-        }
-    }
-
-    return true;
+    return string1 == string2;
 }
 
 string cifra_de_cesar(string texto, int deslocamento) {
@@ -30,7 +25,7 @@ string cifra_de_cesar(string texto, int deslocamento) {
 
     for (int i = 0; i < texto.length(); i++) {
         char c = texto[i];
-        if (isalpha(c)) { // apenas letras
+        if (isalpha(c)) { 
             char base = islower(c) ? 'a' : 'A'; 
             c = (c - base + deslocamento) % 26 + base;
         }
@@ -45,11 +40,14 @@ string encontrar_maior_palavra(string frase) {
 }
 
 int main() {
-    // testando a função de anagramas
-    cout << sao_anagramas("roma", "amor") << endl;
+    assert(sao_anagramas("roma", "amor"));
+    assert(sao_anagramas("Roma", "AMOR")); 
+    assert(!sao_anagramas("banana", "babana")); 
+    assert(!sao_anagramas("abc", "ab"));
 
-    // testando a função de cifra de César
-    cout << cifra_de_cesar("Ataque ao Amanhecer!", 5) << endl;
+    assert(cifra_de_cesar("a", 1) == "b");
+    assert(cifra_de_cesar("z", 1) == "e");
+    assert(cifra_de_cesar("Ataque ao Amanhecer!", 5) == "Fyfvzj ft Frfsmjhjw!");
 
     return 0;
 }
